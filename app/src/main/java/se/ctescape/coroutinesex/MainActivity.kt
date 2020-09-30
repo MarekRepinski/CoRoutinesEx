1e9d6847c1a7a24624dbe577431d4ba5d51f04cc
Marek <repinski@hotmail.com>
2020-09-30 15:41:14 +0200

    Added two more coroutines

--- app/src/main/java/se/ctescape/coroutinesex/MainActivity.kt
+++ app/src/main/java/se/ctescape/coroutinesex/MainActivity.kt
@@ -3,15 +3,21 @@ package se.ctescape.coroutinesex
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
+    private lateinit var text: TextView
     private val RESULT_1 = "Result #1"
+    private val RESULT_2 = "Result #2"
+    private val RESULT_3 = "Result #3"
 
     // Coroutine är ett job i en Thread (det kan finnas flera)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,17 +25,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.button)
+        text = findViewById(R.id.textView)
         button.setOnClickListener {
             //IO för Databaser, nätverk mm, Main är för huvudtråden, Default är för tunggung
             CoroutineScope(IO).launch {
+                //coroutine 1 och 2 startar efter varandra
                 fakeApiRequest()
+            }
+            CoroutineScope(IO).launch {
+                //coroutine 3 startar samtidigt som 1 och blir färdig för coroutine 2
+                setTextOnMainThread(getResult3FromApi())
             }
         }
     }
 
+    private fun setNewText(inStr: String){
+        val newText = text.text.toString() + "\n$inStr"
+        text.text = newText
+    }
+
+    private suspend fun setTextOnMainThread(inStr: String){
+        // detta händer på huvudtråden:
+        withContext(Main){
+            setNewText(inStr)
+        }
+        //Coroutines kan på så sätt hoppa mellan trådar!!!
+    }
+
     private suspend fun fakeApiRequest(){
-        val result1 = getResult1FromApi()
-        println("AIK!!  $result1")
+        setTextOnMainThread(getResult1FromApi())
+        setTextOnMainThread(getResult2FromApi())
     }
 
     //Suspend betyder att den KAN köras i bakrunden
@@ -37,6 +62,18 @@ class MainActivity : AppCompatActivity() {
         logThread("getResult1FromApi")
         delay(1000) //Kommer bara att stoppa coroutenen inte hela tråden..
         return RESULT_1
+    }
+
+    private suspend fun getResult2FromApi(): String{
+        logThread("getResult2FromApi")
+        delay(4000) //Kommer bara att stoppa coroutenen inte hela tråden..
+        return RESULT_2
+    }
+
+    private suspend fun getResult3FromApi(): String{
+        logThread("getResult3FromApi")
+        delay(3000) //Kommer bara att stoppa coroutenen inte hela tråden..
+        return RESULT_3
     }
 
     private fun logThread(methodName: String){
